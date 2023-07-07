5c80f456aa55f0547a265946ad5d0cfce06582f6
farid faridamagua <faridamagua@live.com>
2023-07-07 16:11:36 +0200

    FARID
    SUM/RES/DIV/MULT

--- pract-1.cbl
+++ pract-1.cbl
@@ -14,11 +14,48 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+       01  HI PIC XXXX VALUE "HOLA".
+       01  FECHA PIC 9(4) VALUE 2023.
        01  WELCOME PIC A(40).
+       01  NUM1 PIC 9(4).
+       01  NUM2 PIC 9(4).
+       01  RESULT PIC 9(5).
        PROCEDURE DIVISION.
-           DISPLAY 'Hola'
-           MOVE "WELCOME TO MY PORTFOLIO "TO WELCOME
+           DISPLAY HI" "FECHA.
+           MOVE "WELCOME TO MY PORTFOLIO "TO WELCOME.
            DISPLAY "EVERYONE " WELCOME.
+
+               DISPLAY "SUMA"
+           DISPLAY "ENTER A NUMBER:"
+           ACCEPT NUM1.
+           DISPLAY "ENTER A NUMBER:"
+           ACCEPT NUM2.
+           ADD NUM1 TO NUM2 GIVING RESULT.
+           DISPLAY "RESULT: "RESULT.
+
+               DISPLAY "RESTA"
+           DISPLAY "ENTER A NUMBER:"
+           ACCEPT NUM1.
+           DISPLAY "ENTER A NUMBER:"
+           ACCEPT NUM2.
+           SUBTRACT NUM1 FROM NUM2 GIVING RESULT.
+           DISPLAY "RESULT: "RESULT.
+
+               DISPLAY "MULTIPLE"
+           DISPLAY "ENTER A NUMBER:"
+           ACCEPT NUM1.
+           DISPLAY "ENTER A NUMBER:"
+           ACCEPT NUM2.
+           MULTIPLY NUM1 BY NUM2 GIVING RESULT.
+           DISPLAY "RESULT: "RESULT.
+
+                   DISPLAY "DIVISION"
+           DISPLAY "ENTER A NUMBER:"
+           ACCEPT NUM1.
+           DISPLAY "ENTER A NUMBER:"
+           ACCEPT NUM2.
+           DIVIDE NUM1 BY NUM2 GIVING RESULT.
+           DISPLAY "RESULT: "RESULT.
 
             STOP RUN.
        END PROGRAM Divisiones.
